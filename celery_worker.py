fd2a6f733e1d65d8737400a7f322c1589c74709c
Granit <granit.g4shii@gmail.com>
2025-07-05 10:54:35 +0200

    fixed some bugs on celery

--- celery_worker.py
+++ celery_worker.py
@@ -1,5 +1,5 @@
 from celery import Celery
-
+import os
 celery_app = Celery(
     "worker",
     broker="rediss://default:AWx6AAIjcDFjNGYwZDJkYzBkOWY0ZDE3OTBmZWRiZTkxYjYyYzM2MnAxMA@select-quetzal-27770.upstash.io:6379?ssl_cert_reqs=none",     # Fill this below
@@ -7,3 +7,5 @@ celery_app = Celery(
 )
 
 celery_app.conf.task_track_started = True
+
+from app import tasks
