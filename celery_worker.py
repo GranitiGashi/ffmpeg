99cd2c8fde85c8aa06ff3ef0da26c401b89c7de1
Granit <granit.g4shii@gmail.com>
2025-08-02 11:57:43 +0200

    fixed an issue on celery_worker

--- celery_worker.py
+++ celery_worker.py
@@ -1,5 +1,5 @@
 from celery import Celery
-from app import tasks  # import tasks to register them with celery
+
 
 # Use environment variables or config file to avoid hardcoding sensitive info
 broker_url = "rediss://default:AWx6AAIjcDFjNGYwZDJkYzBkOWY0ZDE3OTBmZWRiZTkxYjYyYzM2MnAxMA@select-quetzal-27770.upstash.io:6379?ssl_cert_reqs=none"
@@ -14,4 +14,4 @@ celery_app = Celery(
 celery_app.conf.task_track_started = True
 
 # This import registers your tasks with Celery
-import app.tasks
+celery_app.autodiscover_tasks(["app"])
